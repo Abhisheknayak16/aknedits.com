afdc291d928e21594d29253d4cc664856d7bac81
Abhisheknayak16 <anayak8543@gmail.com>
2025-02-25 09:58:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-document.querySelectorAll('nav ul li a').forEach(anchor => {
+<script>
+    document.getElementById("toggle-theme").addEventListener("click", function() {
+        document.body.classList.toggle("dark-mode");
+    });
+</script>
+    document.querySelectorAll('nav ul li a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
         const target = document.querySelector(this.getAttribute('href'));
