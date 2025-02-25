0fa47a450d47e1a7c7a414d3ab5796beb2acebbf
Abhisheknayak16 <anayak8543@gmail.com>
2025-02-25 09:28:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,10 @@ document.querySelectorAll('.nav-links a').forEach(link => {
         }
     });
 });
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
